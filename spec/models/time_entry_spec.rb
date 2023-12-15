3fe8482dd2ac7097b3adccf3b71d4a28af21ea10
Delon Newman <contact@delonnewman.name>
2023-12-14 22:43:59 -0700

    fix: update TimeEntry spec

--- spec/models/time_entry_spec.rb
+++ spec/models/time_entry_spec.rb
@@ -2,16 +2,16 @@
 
 describe TimeEntry, type: :model do
   subject(:entry) { described_class.new(project:, minutes:) }
-  let(:project) { Project.create!(description: 'This is a Test Project') }
+  let(:project) { Project.create!(name: 'This is a Test Project', group: Group.create!(name: 'Test Group')) }
   let(:minutes) { 56 }
 
   describe '#valid?' do
-    it 'returns true if activity and minutes are given' do
+    it 'returns true if project and minutes are given' do
       expect(entry).to be_valid
     end
 
-    it 'returns false if activity is not given' do
-      entry.activity_id = nil
+    it 'returns false if project is not given' do
+      entry.project_id = nil
 
       expect(entry).not_to be_valid
     end
