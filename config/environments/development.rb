a5080b6c7546ca40455341d88d7f625ad25ac34e
Delon Newman <contact@delonnewman.name>
2023-12-20 10:56:36 -0700

    Update config and CI

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Need host a port for absolute urls
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.perform_cachin false
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
